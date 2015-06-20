9ad47f4786f5e48125c954f141bfdb4aad49e836
Curtis Larsen <larsen@dixie.edu>
2015-06-19 21:55:20 -0600

    fixed board win description for second diagonal;  changed scaling of material scores so boards are much more valuable than positions

--- src/uttt_ai.py
+++ src/uttt_ai.py
@@ -16,16 +16,18 @@ class Board:
         return
 
     def CheckBoardWin(self, b):
-        for i in range(0, 9, 3):
-            if b[i] == b[i+1] and b[i] == b[i+2] and b[i] != uttt_data.PLAYER_N:
-                return b[i]
-        for i in range(0, 3, 1):
-            if b[i] == b[i+3] and b[i] == b[i+6] and b[i] != uttt_data.PLAYER_N:
-                return b[i]
-        if b[0] == b[4] and b[0] == b[8] and b[0] != uttt_data.PLAYER_N:
-            return b[0]
-        if b[2] == b[4] and b[0] == b[6] and b[0] != uttt_data.PLAYER_N:
-            return b[0]
+        wins = [ [ 0, 1, 2 ],
+                 [ 3, 4, 5 ],
+                 [ 6, 7, 8 ],
+                 [ 0, 3, 6 ],
+                 [ 1, 4, 7 ],
+                 [ 2, 5, 8 ],
+                 [ 0, 4, 8 ],
+                 [ 2, 4, 6 ],
+             ]
+        for w in wins:
+            if b[w[0]] == b[w[1]] and b[w[0]] == b[w[2]] and b[w[0]] != uttt_data.PLAYER_N:
+                return b[w[0]]
         return uttt_data.PLAYER_N
         
     def MakeMove(self, move):
@@ -46,6 +48,15 @@ class Board:
 
         return undo_info
 
+    def UndoMove(self, undo_info):
+        (b, p, m, np, nb, bo, w) = undo_info
+        self.markers[b][p] = m
+        self.next_player = np
+        self.next_board = nb
+        self.board_owners[b] = bo
+        self.winner = w
+        return
+
     def PositionScore(self, position, scores):
         if position in [ 0, 2, 6, 8 ]:
             score = scores[0]
@@ -59,14 +70,14 @@ class Board:
         
         
     def Evaluate(self):
-        GAME_WIN_SCORE = 100000
-        BOARD_CORNER_SCORE = 60
-        BOARD_CENTER_SCORE = 100
-        BOARD_EDGE_SCORE = 30
+        POSITION_EDGE_SCORE = 1
+        POSITION_CORNER_SCORE = POSITION_EDGE_SCORE * 3
+        POSITION_CENTER_SCORE = POSITION_CORNER_SCORE * 3
+        BOARD_EDGE_SCORE = POSITION_EDGE_SCORE * 1000
+        BOARD_CORNER_SCORE = POSITION_CORNER_SCORE * 1000
+        BOARD_CENTER_SCORE = POSITION_CENTER_SCORE * 1000
+        GAME_WIN_SCORE = BOARD_CENTER_SCORE * 1000
         BOARD_SCORES = [ BOARD_CORNER_SCORE, BOARD_CENTER_SCORE, BOARD_EDGE_SCORE ]
-        POSITION_CORNER_SCORE = 6
-        POSITION_CENTER_SCORE = 10
-        POSITION_EDGE_SCORE = 3
         POSITION_SCORES = [ POSITION_CORNER_SCORE, POSITION_CENTER_SCORE, POSITION_EDGE_SCORE ]
         score = 0
         if self.winner != uttt_data.PLAYER_N:
@@ -89,15 +100,6 @@ class Board:
         return score
                     
                     
-
-    def UndoMove(self, undo_info):
-        (b, p, m, np, nb, bo, w) = undo_info
-        self.markers[b][p] = m
-        self.next_player = np
-        self.next_board = nb
-        self.board_owners[b] = bo
-        self.winner = w
-        return
 
     def LegalMoves(self):
         moves = []
@@ -122,24 +124,28 @@ class UTTTAI:
         self.data = data
         self.ai_send_queue = ai_send_queue
         self.ai_recv_queue = ai_recv_queue
+        self.INFINITY = 1000000000
         return
 
     def ChooseMove(self, depth):
+        if self.data.GetState() != uttt_data.STATE_SHOW_GAME:
+            return None
         board = Board(self.data)
-        alpha = -2000000
-        beta = 2000000
-        (move, value) = self.Max(board, depth, alpha, beta)
+        alpha = - 2 * self.INFINITY
+        beta = 2 * self.INFINITY
+        (move, value) = self.Max(board, depth, alpha + 0, beta + 0)
+        print "Chose %s for %d." % (str(move), int(value))
         return move
 
     def Max(self, board, depth, alpha, beta):
         if depth <= 0 or board.winner != uttt_data.PLAYER_N:
             return (None, board.Evaluate())
-        max_value = -1000000
+        max_value = - self.INFINITY
         max_move = None
         moves = board.LegalMoves()
         for m in moves:
             undo_info = board.MakeMove(m)
-            (junk, value) = self.Min(board, depth-1, alpha, beta)
+            (junk, value) = self.Min(board, depth-1, alpha + 0, beta + 0)
             board.UndoMove(undo_info)
             if value > max_value:
                 max_value = value
@@ -153,12 +159,12 @@ class UTTTAI:
     def Min(self, board, depth, alpha, beta):
         if depth <= 0 or board.winner != uttt_data.PLAYER_N:
             return (None, board.Evaluate())
-        min_value = 1000000
+        min_value = self.INFINITY
         min_move = None
         moves = board.LegalMoves()
         for m in moves:
             undo_info = board.MakeMove(m)
-            (junk, value) = self.Max(board, depth-1, alpha, beta)
+            (junk, value) = self.Max(board, depth-1, alpha + 0, beta + 0)
             board.UndoMove(undo_info)
             if value < min_value:
                 min_value = value
@@ -180,6 +186,9 @@ class UTTTAI:
             except Queue.Empty as e:
                 # not ready yet, fine
                 pass
+                
+        board = Board(self.data)
+        print "Final Score:", board.Evaluate()
         return
             
             
